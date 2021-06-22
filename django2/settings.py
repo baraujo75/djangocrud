daa382cd3002a7800bede7b8992fc27538f5ccb7
bruno.bvaraujo@gmail.com <bruno.bvaraujo@gmail.com>
2021-06-22 11:55:21 -0300

    Removed secret key

--- django2/settings.py
+++ django2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u372#g=u^9rwl88w8lm0c_dm2vgqy(r07h-$e-m7#osrh6pfyu'
+SECRET_KEY = 'xxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
